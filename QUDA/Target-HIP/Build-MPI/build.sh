5a4d0a3c753ba64e3b2b0b3839f66f643acb770e
WANG Ting-Xiao (Advisor: LIU Zhaofeng) <wangtx@ihep.ac.cn>
2026-02-06 16:45:03 +0800

    fix bugs and adapted to environment /bin/sh

---
+++ QUDA/Target-HIP/Build-MPI/build.sh
@@ -0,0 +1,158 @@
+#!/bin/sh
+QUDA_HOME=/path/where/you/would/install/quda
+MPI_HOME=/path/where/you/have/installed/mpi
+EIGEN_HOME=/path/where/you/have/installed/eigen3
+# Modify the lines above first!
+unset -f _main_ _check_
+
+_main_() {
+  if [ "X$1X" = 'XX' ]
+  then
+    echo "ERROR: Missing environment variable; 'QIO_HOME' should not be set empty! "
+    return 1
+  fi
+  if [ "X$2X" = 'XX' ]
+  then
+    echo "ERROR: Missing environment variable; 'MPI_HOME' should not be set empty! "
+    return 1
+  fi
+  if [ "X$3X" = 'XX' ]
+  then
+    echo "ERROR: Missing environment variable; 'EIGEN_HOME' should not be set empty! "
+    return 1
+  fi
+  _is_modified='Y'
+  if [ "X$1X" = 'X/path/where/you/would/install/qudaX' ]
+  then
+    echo 'QUDA_HOME=/path/where/you/would/install/quda '
+    _is_modified='N'
+  fi
+  if [ "X$2X" = 'X/path/where/you/have/installed/mpiX' ]
+  then
+    echo 'MPI_HOME=/path/where/you/have/installed/mpi '
+    _is_modified='N'
+  fi
+  if [ "X$3X" = 'X/path/where/you/have/installed/eigen3X' ]
+  then
+    echo 'EIGEN_HOME=/path/where/you/have/installed/eigen '
+    _is_modified='N'
+  fi
+  if [ "X${_is_modified}X" != 'XYX' ]
+  then
+    echo '# Modify the lines above first! '
+    return 1
+  fi
+  { cd -- "$2" && _MPI_PATH=$(pwd) && cd - 1> /dev/null; } || return
+  { cd -- "$3" && _EIGEN_PATH=$(pwd) && cd - 1> /dev/null; } || return
+  { mkdir -p -- "$1" && cd -- "$1"; } || return
+  _PREFIX_=$(pwd) || return
+  REPO_NAME=$(git remote get-url origin 2>/dev/null)
+  cd - 1>/dev/null
+  return 0
+}
+
+_check_() {
+  if [ "X$1X" = 'XX' ]; then return 1; fi
+  cd -- "${1%/*}" 2>/dev/null || return
+  if [ ! -f build.sh ]; then return 1; fi
+  if [ ! -f .gitkeepcache ]; then return 1; fi
+  CURRENT=$(pwd) || return
+  cd - 1>/dev/null
+  return 0
+}
+
+unset CURRENT REPO_HOME REPO_NAME _PREFIX_
+_check_ "$0" || _check_ "${BASH_SOURCE[0]}" || _check_ "./." || {
+ eval 'echo ${.sh.file}' 1>/dev/null 2>&1 && _check_ "${.sh.file}"
+} || {
+ eval 'echo ${(%):-%x}' 1>/dev/null 2>&1 && _check_ "${(%):-%x}"
+}
+if [ "X${CURRENT}X" = 'XX' ]
+then
+  echo "ERROR: Cannot locate this script 'build.sh'. "
+  echo 'Try executing it, rather than sourcing it. '
+  return 1
+fi
+REPO_HOME=$(cat "$CURRENT/.gitkeepcache")
+if [ "X${REPO_HOME}X" = 'XX' ]
+then
+  echo "ERROR: Run 'setup.sh' first! "
+  return 1
+fi
+_main_ "$QUDA_HOME" "$MPI_HOME" "$EIGEN_HOME" || return
+
+_is_install='Y'
+if [ "X${REPO_NAME##*.}X" = 'X.gitX' ]
+then
+  echo "WARNING: 'QUDA_HOME' is in the repository '$REPO_NAME', which is not recommended."
+  printf 'Are you sure to install QUDA in %s ([No]/Yes)? ' "'$_PREFIX_'"
+  read -r _user_option
+  _is_install=$(printf '%s' "$_user_option" | tr '[:lower:]' '[:upper:]')
+fi
+if [ "X${_is_install%ES}X" != 'XYX' ]; then return 0; fi
+if [ ! -f "$REPO_HOME/CMakeLists.txt" ]
+then
+  echo "ERROR: File not found '$REPO_HOME/CMakeLists.txt'. "
+  echo 'Something went wrong! Aborted. '
+  return 1
+fi
+_MPI_PATH="$_MPI_PATH${CMAKE_PREFIX_PATH:+:}"
+CMAKE_PREFIX_PATH="$_MPI_PATH$CMAKE_PREFIX_PATH"
+_CXX_PATH="$(command -v c++)"
+_CXX_LD_LIB="${_CXX_PATH%/bin/*}/lib64"
+_LINKER="-L$_CXX_LD_LIB -Wl,-rpath,$_CXX_LD_LIB"
+cmake -Wno-dev \
+ -S "$REPO_HOME" \
+ -B "$REPO_HOME/build" \
+ -DCMAKE_INSTALL_PREFIX="$_PREFIX_" \
+ -DEIGEN_INCLUDE_DIR="$_EIGEN_PATH/include/eigen3" \
+ -DCMAKE_BUILD_TYPE=RELEASE \
+ -DQUDA_BUILD_SHAREDLIB=ON \
+ -DQUDA_TARGET_TYPE=HIP \
+ -DQUDA_DIRAC_LAPLACE=ON \
+ -DQUDA_DIRAC_COVDEV=ON \
+ -DQUDA_DIRAC_WILSON=ON \
+ -DQUDA_DIRAC_CLOVER=ON \
+ -DQUDA_DIRAC_STAGGERED=ON \
+ -DQUDA_DIRAC_DOMAIN_WALL=OFF \
+ -DQUDA_DIRAC_TWISTED_MASS=OFF \
+ -DQUDA_DIRAC_TWISTED_CLOVER=OFF \
+ -DQUDA_CLOVER_DYNAMIC=ON \
+ -DQUDA_CLOVER_RECONSTRUCT=ON \
+ -DQUDA_CLOVER_CHOLESKY_PROMOTE=ON \
+ -DQUDA_MULTIGRID=ON \
+ -DQUDA_ENABLE_MMA=ON \
+ -DQUDA_MULTIGRID_DSLASH_PROMOTE=ON \
+ -DQUDA_MPI=ON \
+ -DQUDA_QMP=OFF \
+ -DQUDA_QIO=OFF \
+ -DQUDA_ARPACK=OFF \
+ -DQUDA_USE_EIGEN=ON \
+ -DQUDA_DOWNLOAD_EIGEN=OFF \
+ -DQUDA_DOWNLOAD_USQCD=OFF \
+ -DQUDA_DOWNLOAD_NVSHMEM=OFF \
+ -DQUDA_INTERFACE_QDP=ON \
+ -DQUDA_INTERFACE_MILC=OFF \
+ -DQUDA_INTERFACE_CPS=OFF \
+ -DQUDA_INTERFACE_BQCD=OFF \
+ -DQUDA_INTERFACE_TIFR=OFF \
+ -DQUDA_INTERFACE_OPENQCD=OFF \
+ -DQUDA_SPACK_BUILD=OFF \
+ -DQUDA_BACKWARDS=OFF \
+ -DQUDA_PRECISION=14 \
+ -DQUDA_RECONSTRUCT=7 \
+ -DQUDA_CXX_STANDARD=17 \
+ -DQUDA_MAX_MULTI_BLAS_N=8 \
+ -DCMAKE_C_COMPILER="$(command -v clang)" \
+ -DCMAKE_CXX_COMPILER="$(command -v clang++)" \
+ -DCMAKE_HIP_COMPILER="$(command -v dcc)" \
+ -DMPI_C_COMPILER="$(command -v mpicc)" \
+ -DMPI_CXX_COMPILER="$(command -v mpicxx)" \
+ -DCMAKE_C_FLAGS='-O3 -fPIC' \
+ -DCMAKE_CXX_FLAGS='-O3 -fPIC' \
+ -DCMAKE_EXE_LINKER_FLAGS="$_LINKER" \
+ -DCMAKE_SHARED_LINKER_FLAGS="$_LINKER"
+if [ $? -ne 0 ]; then return; fi
+_nproc=$(( $(nproc) / 3 ))
+make -C "$REPO_HOME/build" -j$_nproc || return
+make -C "$REPO_HOME/build" install || return
